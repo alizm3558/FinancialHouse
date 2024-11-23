cbcf01f2d0f3204bd52617ce1462ea54921b4cd5
alizm3558 <alibakir3558@gmail.com>
2024-11-24 01:02:09 +0300

    Kalan Unit test yazıldı

--- src/test/java/com/alibakir/financialHouse/TransactionReportControllerTest.java
+++ src/test/java/com/alibakir/financialHouse/TransactionReportControllerTest.java
@@ -3,15 +3,13 @@ package com.alibakir.financialHouse;
 
 
 import com.alibakir.financialHouse.controller.TransactionReportController;
-import com.alibakir.financialHouse.domain.Dto.ClientDto;
+import com.alibakir.financialHouse.domain.Dto.ReportRequestDto;
 import com.alibakir.financialHouse.domain.Dto.TransactionRequestDto;
 import com.alibakir.financialHouse.service.AuthService;
 import com.alibakir.financialHouse.service.RequestService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -43,6 +41,9 @@ class TransactionReportControllerTest {
     @MockBean
     private RequestService requestService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     void testGetClient() throws Exception {
         String transactionId = "1010993-1539329656-1293";
@@ -56,6 +57,54 @@ class TransactionReportControllerTest {
 
         System.out.println("---Test Başarılı---");
 
+    }
+
+    @Test
+    void testGetTransactionReport() throws Exception {
+
+        ReportRequestDto requestDto = new ReportRequestDto();
+        requestDto.setFromDate("2015-01-01");
+        requestDto.setToDate("2024-01-31");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/transactions/report")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+
+
+        System.out.println("---Test Başarılı---");
 
     }
+    @Test
+    void testGetTransactionList() throws Exception {
+
+        ReportRequestDto requestDto = new ReportRequestDto();
+        requestDto.setFromDate("2015-01-01");
+        requestDto.setToDate("2024-01-31");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/transactions/list")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+
+
+        System.out.println("---Test Başarılı---");
+
+    }
+
+    @Test
+    void testGetTransaction() throws Exception {
+        String transactionId = "1010993-1539329656-1293";
+
+        TransactionRequestDto requestDto = new TransactionRequestDto();
+        requestDto.setTransactionId(transactionId);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/transaction")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"transactionId\": \"" + transactionId + "\"}"))
+                .andExpect(status().isOk());
+
+        System.out.println("---Test Başarılı---");
+
+    }
+
 }
