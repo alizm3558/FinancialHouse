7cfdedd39f1ca64d8bf828bc778cb4f1a4a8236e
alizm3558 <alibakir3558@gmail.com>
2024-11-24 16:12:51 +0300

    CORS düzenlemeleri yapıldı, istek gelen IP adresi loglaması gerçekleştirildi

--- src/main/java/com/alibakir/financialHouse/controller/TransactionReportController.java
+++ src/main/java/com/alibakir/financialHouse/controller/TransactionReportController.java
@@ -5,11 +5,14 @@ import com.alibakir.financialHouse.domain.Dto.*;
 import com.alibakir.financialHouse.service.AuthService;
 import com.alibakir.financialHouse.service.RequestService;
 import io.swagger.v3.oas.annotations.Operation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Value;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 
@@ -22,6 +25,8 @@ public class TransactionReportController {
     private final AuthService authService;
 
     private final RequestService requestService;
+
+    private static final Logger logger = LoggerFactory.getLogger(TransactionReportController.class);
 
     @Value("${api.report.url}")
     private String reportUrl;
@@ -38,8 +43,10 @@ public class TransactionReportController {
 
     @Operation(summary ="https://sandbox-reporting.rpdpymnt.com/api/v3/client")
     @PostMapping("/client")
-    public ResponseEntity<ClientDto> getClient(@Valid @RequestBody TransactionRequestDto requestDto) {
+    public ResponseEntity<ClientDto> getClient(@Valid @RequestBody TransactionRequestDto requestDto, HttpServletRequest request) {
         try {
+            String clientIp = request.getRemoteAddr();
+            logger.info("Gelen isteğin IP adresi: {}", clientIp);
             String token = authService.getValidToken();
 
             ClientDto clientDto = requestService.getClient(token, requestDto.getTransactionId());
@@ -54,8 +61,10 @@ public class TransactionReportController {
 
     @Operation(summary ="https://sandbox-reporting.rpdpymnt.com/api/v3/transactions/report")
     @PostMapping("/transactions/report")
-    public ResponseEntity<ReportDto> getTransactionReport(@Valid @RequestBody ReportRequestDto requestDto) {
+    public ResponseEntity<ReportDto> getTransactionReport(@Valid @RequestBody ReportRequestDto requestDto, HttpServletRequest request) {
         try {
+            String clientIp = request.getRemoteAddr();
+            logger.info("Gelen isteğin IP adresi: {}", clientIp);
 
             String token = authService.getValidToken();
 
@@ -71,8 +80,10 @@ public class TransactionReportController {
     @Operation(summary ="https://sandbox-reporting.rpdpymnt.com/api/v3/transaction/list")
     @PostMapping("/transactions/list")
     public ResponseEntity<TransactionResponseDto> getTransactionList(
-            @Valid @RequestBody ReportRequestDto requestDto) {
+            @Valid @RequestBody ReportRequestDto requestDto, HttpServletRequest request) {
         try {
+            String clientIp = request.getRemoteAddr();
+            logger.info("Gelen isteğin IP adresi: {}", clientIp);
             String token = authService.getValidToken();
 
             TransactionResponseDto transactionResponseDto = requestService.getTransactionList(token, requestDto);
@@ -87,8 +98,10 @@ public class TransactionReportController {
 
     @Operation(summary ="https://sandbox-reporting.rpdpymnt.com/api/v3/transaction")
     @PostMapping("/transaction")
-    public ResponseEntity<GeneralTransactionResponseDto> getTransaction(@Valid @RequestBody TransactionRequestDto requestDto) {
+    public ResponseEntity<GeneralTransactionResponseDto> getTransaction(@Valid @RequestBody TransactionRequestDto requestDto, HttpServletRequest request) {
         try {
+            String clientIp = request.getRemoteAddr();
+            logger.info("Gelen isteğin IP adresi: {}", clientIp);
             String token = authService.getValidToken();
 
             GeneralTransactionResponseDto transactionDto = requestService.getTransactions(token, requestDto.getTransactionId());
